60fc112a705910133c77b4ceb18c1d7dcc3a37a8
Anderson18MD <160438512+Anderson18MD@users.noreply.github.com>
2024-03-01 13:16:30 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Anderson18MD', // Usually your GitHub org/user name.
   projectName: 'caso2.gitub.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages' ,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
